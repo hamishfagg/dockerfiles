11ec6cc34523231fc7561b33fe1701abc8c82721
RealityFork <mystx@chaostheory.co.nz>
2018-08-05 13:37:31 +1200

    added uploadr

---
+++ uploadr/server.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# Reflects the requests from HTTP methods GET, POST, PUT, and DELETE
+# Written by Nathan Hamiel (2010)
+
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from optparse import OptionParser
+import cgi
+from subprocess import call
+
+class RequestHandler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+
+        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+        if ctype == 'multipart/form-data':
+            postvars = cgi.parse_multipart(self.rfile, pdict)
+        elif ctype == 'application/x-www-form-urlencoded':
+            length = int(self.headers.getheader('content-length'))
+            postvars = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
+        else:
+            postvars = {}
+
+        call(["/bin/sh", "-c", "wget -O /tmp/upfile " + postvars["url"][0]])
+        call(["/bin/sh", "-c", "./uploadr.py --notify " + postvars["notify"][0] + " /tmp/upfile"])
+        self.send_response(200)
+
+def main():
+    port = 8080
+    print('Listening on localhost:%s' % port)
+    server = HTTPServer(('', port), RequestHandler)
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.usage = ("Creates an http-server that will echo out any GET or POST parameters\n"
+                    "Run:\n\n"
+                    "   reflect")
+    (options, args) = parser.parse_args()
+
+    main()
